729acfe7116043ce0d3cd5184cdb6c38f893b4a5
azrap <azraap@gmail.com>
2020-01-17 10:26:04 -0800

    all answers are passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,9 +9,31 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
+    index = 0
+
+    if length == 1:
+        return None
+
+    while index < length:
+        hash_table_insert(ht, weights[index], index)
+        # print('weights index', weights[index], index)
+        index += 1
+
     """
     YOUR CODE HERE
     """
+    for w1 in weights:
+        w2 = limit - w1
+
+        w1_idx = hash_table_retrieve(ht, w1)
+        w2_idx = hash_table_retrieve(ht, w2)
+        if w2_idx:
+            if w1 == w2:
+                answer = (1, 0)
+            else:
+                answer = (w2_idx, w1_idx)
+
+            return answer
 
     return None
 
